c8e23037a0f8d8497cc248fd38e0a6ef8c0098bb
ywq <1320388284@qq.com>
2021-12-05 19:38:03 +0800

    feat: 增加表格组件

---
+++ src/components/ElTablePagination.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="el-table-pagination-wrapper">
+    <el-table
+      :data="tableParams.tableData"
+      height="100%"
+      ref="innerTable"
+      :row-key="getRowKeys"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column
+        v-if="selection"
+        :reserve-selection="reserveSelection"
+        type="selection"
+        width="55"
+      >
+      </el-table-column>
+      <template v-for="(item, index) in columns">
+        <el-table-column
+          v-if="!item.hidden && item.type !== 'slot'"
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+          :align="item.align||'center'"
+          :render-header="item.renderHeader"
+          :min-width="item.minWidth"
+          :width="item.width"
+          show-overflow-tooltip
+        >
+          <template v-slot="scope">
+            <span>{{ scope.row[item.prop] || "-" }}</span>
+          </template>
+        </el-table-column>
+        <slot v-else-if="item.type === 'slot'" :name="item.slotName"/>
+      </template>
+    </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-size="pageSize"
+      :page-sizes="[10, 30, 50]"
+      :total="tableParams.pageTotal"
+      layout="total, sizes, prev, pager, next, jumper"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch, defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  columns: Array,
+  pageParams: {
+    default: {}
+  },
+  selection: {
+    default: true
+  },
+  reserveSelection: {
+    default: false
+  }
+});
+const tableParams = ref<any>({});
+const currentPage = ref(1);
+const pageSize = ref(10);
+
+const emit = defineEmits(["handleSelectionChange", "handleSizeChange", "handleCurrentChange"]);
+
+watch(
+  props.pageParams,
+  value => {
+    tableParams.value = Object.assign({}, value);
+    currentPage.value = tableParams.value.currentPage;
+    pageSize.value = tableParams.value.pageSize;
+  },
+  {
+    immediate: true,
+    deep: true
+  }
+);
+
+const handleSelectionChange = (row: any) => {
+  emit("handleSelectionChange", row);
+};
+const handleSizeChange = (val: number) => {
+  emit("handleSizeChange", val);
+};
+const handleCurrentChange = (val: number) => {
+  emit("handleCurrentChange", val);
+};
+
+const getRowKeys = (row: any) => {
+  if (row.id) {
+    return row.id;
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.el-table-pagination-wrapper {
+  height: 100%;
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  background: rgba(47, 65, 86, .7);
+
+  .el-table {
+    background: transparent;
+  }
+
+  .el-pagination {
+    padding: 12px 0;
+    margin: 0;
+    display: flex;
+    justify-content: flex-end;
+    flex: 0 0 52px;
+  }
+}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,117 @@
 <template>
-  <div class="main-page"></div>
+  <div class="main-page">
+    <el-table-pagination
+      :columns="columns"
+      v-loading="loading"
+      element-loading-text="加载中"
+      ref="table"
+      :pageParams="pageParams"
+      :reserveSelection="true"
+      @handleSelectionChange="handleSelectionChange"
+      @handleSizeChange="handleSizeChange"
+      @handleCurrentChange="handleCurrentChange"
+    >
+      <template #operate>
+        <el-table-column
+          label="操作"
+          align="center"
+          width="160"
+        >
+          <template v-slot="scope">
+            <el-button>删除{{ scope.row.name }}</el-button>
+          </template>
+        </el-table-column>
+      </template>
+    </el-table-pagination>
+  </div>
 </template>
+<script lang="ts" setup>
+import ElTablePagination from "@/components/ElTablePagination.vue";
+import { ref, onMounted } from "vue";
+
+const loading = ref(false);
+let selection = ref([]);
+const pageParams = ref({
+  currentPage: 1,
+  pageSize: 10,
+  pageTotal: 0,
+  tableData: []
+});
+const handleSelectionChange = (rows: any) => {
+  selection = rows;
+};
+const handleSizeChange = (val: number) => {
+  pageParams.value.currentPage = 1;
+  pageParams.value.pageSize = val;
+  getData();
+};
+const handleCurrentChange = (val: number) => {
+  pageParams.value.currentPage = val;
+  getData();
+};
+const getData = async () => {
+  const request = () => new Promise((res, rej) => {
+    setTimeout(() => {
+      res([
+        {
+          name: "ywq",
+          sex: "男",
+          age: "18",
+          university: "大学",
+          education: "本科"
+        },
+        {
+          name: "小工",
+          sex: "男",
+          age: "18",
+          university: "大学",
+          education: "本科"
+        },
+        {
+          name: "小红",
+          sex: "女",
+          age: "18",
+          university: "大学",
+          education: "本科"
+        },
+        {
+          name: "小军",
+          sex: "男",
+          age: "26",
+          university: "",
+          education: "本科"
+        }
+      ]);
+    }, 1000);
+  });
+  loading.value = true;
+  const data = await request() as any;
+  loading.value = false;
+  pageParams.value.tableData = data;
+  pageParams.value.pageTotal = 30;
+};
+const columns = [
+  {width: "", label: "姓名", prop: "name"},
+  {width: "", label: "性别", prop: "sex"},
+  {width: "", label: "年龄", prop: "age"},
+  {width: "", label: "毕业学校", prop: "university"},
+  {width: "", label: "学历", prop: "education"},
+  {
+    width: "",
+    type: "slot",
+    label: "操作",
+    slotName: "operate"
+  }
+];
+onMounted(() => {
+  getData();
+});
+</script>
 <style lang="scss" scoped>
 .main-page {
   width: 100%;
   height: 100%;
-  background: url("~@/assets/bk_2.jpg") 0 -78px no-repeat;
+  background: url("~@/assets/bk_2.jpg") 0 -95px no-repeat;
   background-size: cover;
 }
 </style>
